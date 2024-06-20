d4354f0b925a9164c701ba789418af14ae03002d
williamsetiawan65 <williamsetiawan65@students.amikom.ac.id>
2024-06-20 21:11:52 +0700

    github action diperbarui

--- apiAjax.php
+++ apiAjax.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
-$url = "http://openrecipe.000webhostapp.com/api.php?request=".$_GET["par1"]."&option=".$_GET["par2"]."&limit=".$_GET["par3"]."&offset=".$_GET["par4"];
+$url = "http://openrecipe.000webhostapp.com/deploy-test/api.php?request=".$_GET["par1"]."&option=".$_GET["par2"]."&limit=".$_GET["par3"]."&offset=".$_GET["par4"];
 $result = file_get_contents($url);
 echo $result;
 ?>
