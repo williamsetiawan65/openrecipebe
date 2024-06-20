00e5646d5f7ac402676cd510a737c4a52730ce25
williamsetiawan65 <williamsetiawan65@students.amikom.ac.id>
2024-06-20 22:06:02 +0700

    github action diperbarui

--- deploy/api-test.php
+++ deploy/api-test.php
@@ -2,7 +2,7 @@
 
 // version
 // ganti ke mode development untuk menggunakan konfigurasi database lokal, ganti ke mode release untuk menggunakan konfigurasi database di hosting
-$mode = "releases"; // development / release
+$mode = "release"; // development / release
 
 // database connection
 if ($mode == "development") {
